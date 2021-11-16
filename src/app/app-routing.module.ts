c808abf81ee87d506cce91607ea90e6805550a2d
AntonioAlvarado21 <jcruza1300@alumno.ipn.mx>
2021-11-15 18:10:55 -0600

    Formulario de compra con estilo SCSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Codes } from './pages/codes/codes.component';
 
 const routes: Routes = [
   { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
+  { path: 'checkout', loadChildren: () => import('./pages/checkout/checkout.module').then(m => m.CheckoutModule) },
   {path: '**', redirectTo: '', pathMatch: 'full'}, //Las rutas deben estar en orden y poner al ultimo la ruta de error
 ];
 
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { tap } from 'rxjs';
+import { ShoppingCartService } from 'src/app/shared/services/shopping-cart.service';
 import { Product } from './interfaces/product.interface';
 import { ProductsService } from './services/products.service';
 
 @Component({
   selector: 'app-products',
-  templateUrl: './products.component.html',
+  template: `
+  <section class="products">
+    <app-product
+    (addToCartClick)="addToCart($event)" 
+    [product]="product" 
+    *ngFor="let product of products">    
+    </app-product>
+  </section>`,
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
   products!:Product[];
-  constructor(private productSvc: ProductsService) { }
+  constructor(private productSvc: ProductsService, private shoppingCartSvc: ShoppingCartService) { }
 
   ngOnInit(): void {
   
@@ -20,6 +28,11 @@ export class ProductsComponent implements OnInit {
     )
     .subscribe();
 
+  }
+
+  addToCart(product:Product):void{
+    console.log('Add to cart', product);
+    this.shoppingCartSvc.updateCart(product);
   }
 
 }
